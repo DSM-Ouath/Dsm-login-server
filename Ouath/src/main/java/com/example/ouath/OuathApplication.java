ba80679e6061fad21dc1454d7f8df4c628cd2b07
도경 <dokyungkim2926@gmail.com>
2024-03-08 19:57:44 +0900

    .

---
+++ Ouath/src/main/java/com/example/ouath/OuathApplication.java
@@ -0,0 +1,13 @@
+package com.example.ouath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuathApplication.class, args);
+    }
+
+}
---
+++ Ouath/src/test/java/com/example/ouath/OuathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ouath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
