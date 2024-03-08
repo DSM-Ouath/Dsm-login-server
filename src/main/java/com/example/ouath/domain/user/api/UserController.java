da3f09dd2932adef386853a47dd98dc89e8c87e9
dkflfkd53 <b57062562@gmail.com>
2024-03-09 08:39:28 +0900

    🌧 :: userController 추가

---
+++ src/main/java/com/example/ouath/domain/user/api/UserController.java
@@ -0,0 +1,11 @@
+package com.example.ouath.domain.user.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+}
