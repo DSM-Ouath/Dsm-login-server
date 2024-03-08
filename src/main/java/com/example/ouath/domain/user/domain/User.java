a1c9e7b0a83fc8e2c1bbfa698b430c5589da4f78
dkflfkd53 <b57062562@gmail.com>
2024-03-08 22:49:09 +0900

    🌧 :: user entity 추가

---
+++ src/main/java/com/example/ouath/domain/user/domain/User.java
@@ -0,0 +1,16 @@
+package com.example.ouath.domain.user.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+}
