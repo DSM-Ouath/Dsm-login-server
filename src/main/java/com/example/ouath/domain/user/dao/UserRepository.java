79da3ce5af47f1435ac07e2a042affef67227da3
dkflfkd53 <b57062562@gmail.com>
2024-03-09 09:03:05 +0900

    🌧 :: findByAccountId 메서드 추가

--- src/main/java/com/example/ouath/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/ouath/domain/user/dao/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.ouath.domain.user.dao;
 import com.example.ouath.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByAccountId(String accountId);
 }
