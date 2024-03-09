5a466c66c8c28b47272aa4958a29e298eb1541e9
dkflfkd53 <b57062562@gmail.com>
2024-03-09 14:24:27 +0900

    🌧 :: 아이디 중복 확인 api service 추가

---
+++ src/main/java/com/example/ouath/domain/user/application/AccountIdDuplicatedCheckService.java
@@ -0,0 +1,22 @@
+package com.example.ouath.domain.user.application;
+
+import com.example.ouath.domain.user.dao.UserRepository;
+import com.example.ouath.domain.user.dto.response.DuplicateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountIdDuplicatedCheckService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public DuplicateResponse accountIdDuplicatedCheck(String accountId) {
+        if(userRepository.existsByAccountId(accountId)) {
+            return new DuplicateResponse(true);
+        }
+        else return new DuplicateResponse(false);
+    }
+}
