f306f49fa03806dee53df532ac0fb854c33875cc
dkflfkd53 <b57062562@gmail.com>
2024-03-09 14:52:00 +0900

    🌧 :: UserDataRequest 추가

---
+++ src/main/java/com/example/ouath/domain/user/dto/request/UserDataRequest.java
@@ -0,0 +1,17 @@
+package com.example.ouath.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserDataRequest {
+
+    @NotBlank
+    private String accountId;
+
+    @NotBlank
+    private String password;
+}
