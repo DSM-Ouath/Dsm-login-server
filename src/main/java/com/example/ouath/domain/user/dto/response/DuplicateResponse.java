89d356928d82687e6ae2df72dfdcd12717680ab6
dkflfkd53 <b57062562@gmail.com>
2024-03-09 14:13:02 +0900

    🌧 :: DuplicatedResponse 추가

---
+++ src/main/java/com/example/ouath/domain/user/dto/response/DuplicateResponse.java
@@ -0,0 +1,11 @@
+package com.example.ouath.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DuplicateResponse {
+
+    private Boolean duplicated;
+}
