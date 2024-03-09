d7e1ce3ae8e91358229102a4775110ec14a01e55
dkflfkd53 <b57062562@gmail.com>
2024-03-09 09:01:00 +0900

    🌧 :: ConfigurationScan 추가

--- src/main/java/com/example/ouath/OuathApplication.java
+++ src/main/java/com/example/ouath/OuathApplication.java
@@ -2,8 +2,10 @@ package com.example.ouath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OuathApplication {
 
     public static void main(String[] args) {
