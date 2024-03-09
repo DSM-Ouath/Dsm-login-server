ce7b351ae81c4f084312dbbed7eeb9afa4e0f6bb
dkflfkd53 <b57062562@gmail.com>
2024-03-09 14:52:11 +0900

    🌧 :: 유저 정보 조회 api service 추가

---
+++ src/main/java/com/example/ouath/domain/user/application/QueryUserDataService.java
@@ -0,0 +1,41 @@
+package com.example.ouath.domain.user.application;
+
+import com.example.ouath.domain.user.dao.UserRepository;
+import com.example.ouath.domain.user.domain.User;
+import com.example.ouath.domain.user.dto.request.UserDataRequest;
+import com.example.ouath.domain.user.dto.response.UserDataResponse;
+import com.example.ouath.domain.user.exception.PasswordMisMatchException;
+import com.example.ouath.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class QueryUserDataService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public UserDataResponse queryUserDate(UserDataRequest request) {
+
+        User user = userRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
+
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) throw PasswordMisMatchException.EXCEPTION;
+
+        return UserDataResponse.builder()
+                .account_id(user.getAccountId())
+                .password(user.getPassword())
+                .name(user.getName())
+                .password(user.getPassword())
+                .grade(user.getGrade())
+                .class_num(user.getClassNum())
+                .num(user.getNum())
+                .user_role(user.getRole())
+                .build();
+    }
+}
