667efe981d3a1809096a86c2cb0b23538a8414c2
dkflfkd53 <b57062562@gmail.com>
2024-03-09 14:12:34 +0900

    🌧 :: 학번 중복 api 컨트롤러 추가

--- src/main/java/com/example/ouath/domain/user/api/UserController.java
+++ src/main/java/com/example/ouath/domain/user/api/UserController.java
@@ -1,10 +1,14 @@
 package com.example.ouath.domain.user.api;
 
+import com.example.ouath.domain.user.application.ClassNumberDuplicatedCheckService;
 import com.example.ouath.domain.user.application.SignupService;
 import com.example.ouath.domain.user.dto.request.SignupRequest;
+import com.example.ouath.domain.user.dto.response.DuplicateResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +22,17 @@ public class UserController {
 
     private final SignupService signupService;
 
+    private final ClassNumberDuplicatedCheckService classNumberDuplicatedCheckService;
+
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/signup")
     public void signup(@RequestBody @Valid SignupRequest request) {
         signupService.signup(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{classNumber}")
+    public DuplicateResponse classNumberDuplicatedCheck(@PathVariable Integer classNumber) {
+        return classNumberDuplicatedCheckService.classNumberDuplicatedCheck(classNumber);
     }
 }
