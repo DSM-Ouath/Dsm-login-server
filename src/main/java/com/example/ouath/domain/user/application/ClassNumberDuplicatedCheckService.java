b67682e3d46d38fcbef232ec574f6782d769c399
dkflfkd53 <b57062562@gmail.com>
2024-03-09 14:12:49 +0900

    🌧 :: 학번 중복 확인 api service 추가

---
+++ src/main/java/com/example/ouath/domain/user/application/ClassNumberDuplicatedCheckService.java
@@ -0,0 +1,22 @@
+package com.example.ouath.domain.user.application;
+
+import com.example.ouath.domain.user.dao.UserRepository;
+import com.example.ouath.domain.user.dto.response.DuplicateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ClassNumberDuplicatedCheckService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public DuplicateResponse classNumberDuplicatedCheck(Integer classNumber) {
+        if(userRepository.existsByGradeAndClassNumAndNum(classNumber/1000, classNumber%1000/100, classNumber%100)) {
+            return new DuplicateResponse(true);
+        }
+        else return new DuplicateResponse(false);
+    }
+}
