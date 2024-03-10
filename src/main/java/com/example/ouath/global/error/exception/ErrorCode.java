afce0282311592928a7f33947594295ed774a856
dkflfkd53 <b57062562@gmail.com>
2024-03-10 17:11:54 +0900

    🌧 :: errorCode 수정

--- src/main/java/com/example/ouath/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/ouath/global/error/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     // user
     USER_NOT_FOUND(404, "일치하는 유저가 존재하지 않습니다."),
     PASSWORD_MIS_MATCH(401, "비밀번호가 일치하지 않습니다."),
+    INVALID_USER(401, "인증되지 않은 사용자 입니다."),
 
     // jwt
     EXPIRED_TOKEN(401, "만료된 토큰입니다."),
@@ -20,10 +21,7 @@ public enum ErrorCode {
 
     // general
     BAD_REQUEST(400, "프론트 탓..."),
-    INTERNAL_SERVER_ERROR(500, "서버 탓..."),
-
-    // key
-    INVALID_KEY(401, "잘못된 key입니다.");
+    INTERNAL_SERVER_ERROR(500, "서버 탓...");
 
     private final int statusCode;
     private final String message;
