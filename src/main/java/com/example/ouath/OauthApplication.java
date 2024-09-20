dd12d00e1da95b83d7c15f6a9732e3b7949b42ae
dkflfkd53 <b57062562@gmail.com>
2024-09-20 12:20:53 +0900

    hotfix

--- src/main/java/com/example/ouath/OauthApplication.java
+++ src/main/java/com/example/ouath/OauthApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class OuathApplication {
+public class OauthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OuathApplication.class, args);
+        SpringApplication.run(OauthApplication.class, args);
     }
 
 }
